c131d323c815af1b5f318359570f8e4e8f7120e3
Dimittri Souza <dimittrisouza@gmail.com>
2024-08-13 21:49:35 -0300

    Message component improvements.

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:push_notification/chat/chat_service.dart';
+import 'package:push_notification/core/models/chat_message.dart';
 
 class Messages extends StatelessWidget{
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Mensagens"),
+    return StreamBuilder<List<ChatMessage>>(
+        stream: ChatService().messageStream(),
+        builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else if(!snapshot.hasData || snapshot.data!.isEmpty){
+            return Center(
+              child: Text("Sem dados. Vamos converar?"),
+            );
+          }else {
+            final msgs = snapshot.data!;
+            return ListView.builder(
+              reverse: true,
+              itemCount: msgs.length,
+                itemBuilder: (ctx, i) => Text(msgs[i].text)
+            );
+          }
+        }
     );
   }
 }
